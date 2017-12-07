6a9ca8c2b22f1f3f61e8de2d815ff7fc961474ea
celinekoe <celine.koe@annotateconsulting.com>
2017-12-07 16:47:03 +0800

    Added project, added d dummy navbar

---
+++ ui.apps/src/main/content/jcr_root/apps/wrs-clone/components/page/footer/footer.js
@@ -0,0 +1,31 @@
+use(function() {
+
+    var CONST = {
+        PROP_COPYRIGHT_MESSAGE: "copyrightMessage",
+    };
+
+    var rootPage = currentPage.getAbsoluteParent(1);
+    var rootPageResource = rootPage.getContentResource();
+    var rootPageNode = rootPageResource.adaptTo(Packages.javax.jcr.Node);
+
+    var copyrightMessage = null;
+
+    if (rootPageNode.hasProperty(CONST.PROP_COPYRIGHT_MESSAGE)) {
+      copyrightMessage = rootPageNode.getProperty(CONST.PROP_COPYRIGHT_MESSAGE).getString();
+    }
+
+    var calendar = java.util.Calendar.getInstance();
+    var year = calendar.get(java.util.Calendar.YEAR).toString();
+
+    var newMessage = copyrightMessage;
+    newMessage = replaceYear(copyrightMessage);
+
+    return {
+        message: newMessage,
+    };
+
+    function replaceYear(message) {
+      var replacedMessage = message.replace("{ year }", year);
+      return replacedMessage;
+    }
+});
